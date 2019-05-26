8707d219cc48259f4bebffeb52130f05234a8411
Kaenoru <kaenoru@gmail.com>
2019-05-26 06:19:43 +0300

    Table view created and assets uploaded

---
+++ coder-swag/coder-swag/View/CategoryCell.swift
@@ -0,0 +1,29 @@
+//
+//  CategoryCell.swift
+//  coder-swag
+//
+//  Created by Maksim Okala-Kulak on 5/26/19.
+//  Copyright © 2019 Maksim Okala-Kulak. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryTitle: UILabel!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
